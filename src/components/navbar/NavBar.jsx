d749f0dc5041ecbaf640ab40027ba2540ffcc39f
Kamsi Kodi <kamsikodi@gmail.com>
2024-05-10 21:31:52 +0100

    Added links

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import Links "./links/Links";
+
+const NavBar = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>
+                <Links />
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
