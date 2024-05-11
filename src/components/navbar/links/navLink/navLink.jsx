4e0fbda4a5271c8716fbddb48cfc02ee4b5d29ce
Kamsi Kodi <kamsikodi@gmail.com>
2024-05-11 12:21:02 +0100

    Updated global CSS

--- src/components/navbar/links/navLink/navLink.jsx
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -1,12 +1,15 @@
+"use client"
 
 import styles from "./navLink.module.css";
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 const NavLink = ({item}) => {
 
     const pathName = usePathname();
 
     return (
-        <Link href={item.path} className={`${styles.container} `}>
+        <Link href={item.path} className={`${styles.container} ${pathName === item.path && styles.active}`}>
             {item.title}
         </Link>
     );
